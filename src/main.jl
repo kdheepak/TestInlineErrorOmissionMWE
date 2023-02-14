76c54f43f2d3c94737fe3e4a80b4be8060b616fb
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-14 08:27:23 -0500

    Update files

--- src/main.jl
+++ src/main.jl
@@ -1,16 +1,12 @@
 ReadDataSetFromHDF5(name::String) = throw("$name not found")
 
-create_data1(year) = (; data1=Data(; year))
-create_data2(year) = (; data1=Data())
+create_data1() = (; data1=Data(; year=1))
+create_data2() = (; data1=Data())
 
 function main1(years=1:5)
-  for year in years
-    (; data1) = create_data1(year)
-  end
+  create_data1()
 end
 
 function main2(years=1:5)
-  for year in years
-    (; data1) = create_data2(year)
-  end
+  create_data2()
 end
